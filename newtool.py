3a4a7f76f9d7d03d00bce58466017229c43764f7
Jason Whittle <jason.j.whittle@gmail.com>
2023-06-29 17:26:28 +1000

    up

---
+++ newtool.py
@@ -0,0 +1,13 @@
+# Username is used for finding your folder eg. c/users/whittlj2
+username = 'whittlj2'
+
+# Path for input data
+path = 'input.xlsx'
+
+# Output filetype for CCL and authview code
+filetype = '.txt'
+
+#import libraries
+import os
+import datetime
+import pandas as pd
